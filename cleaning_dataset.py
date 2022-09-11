23dc4271bdd65e260483feed527cc7a810200553
valepoliba <v.botta@studenti.poliba.it>
2022-09-11 19:15:58 +0200

    cleaning ds

---
+++ cleaning_dataset.py
@@ -0,0 +1,18 @@
+try:
+    input_file = open('dataset/bank-additional-full.csv', 'r')
+
+    input_file = ''.join([i for i in input_file])
+
+    input_file = input_file.replace('"', '')
+    input_file = input_file.replace(';', ',')
+    input_file = input_file.replace('emp.var.rate', 'emp_var_rate')
+    input_file = input_file.replace('cons.price.idx', 'cons_price_idx')
+    input_file = input_file.replace('cons.conf.idx', 'cons_conf_idx')
+    input_file = input_file.replace('nr.employed', 'nr_employed')
+
+    output_file = open('dataset/bank-full.csv', 'w')
+    output_file.writelines(input_file)
+    output_file.close()
+except:
+    print('An error is occurred while the dataset was processing')
+
