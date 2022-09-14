fc328a3c2c589e9e8e93de1045768a455e0722ef
valepoliba <v.botta@studenti.poliba.it>
2022-09-14 20:44:56 +0200

    cleaning ds done

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+np.random.seed(42)
+
+df = pd.read_csv('dataset/bank-full.csv')
+"""print(df.describe())
+print(df)"""
+
+"""CONVERT CATEGORICAL TO NUMERICAL"""
+le = LabelEncoder()
+"""education, contact', 'month', 'day_of_week',"""
+list_to_convert = ['job', 'marital', 'default', 'housing', 'loan', 'poutcome', 'y']
+for x in list_to_convert:
+    label = le.fit_transform(df[x])
+    df.drop(x, axis=1, inplace=True)
+    df[x] = label
+    print(df[x])
+    print('\n')
+
+df.to_csv('dataset/test.csv')
