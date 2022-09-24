7cb88a6f15c4235683d029705a922e86093f905f
vale06 <v.botta@studenti.poliba.it>
2022-09-24 12:46:30 +0200

    switch to windows

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from logistic_regression import LogisticRegression
 from neural_network import NeuralNetwork
 
@@ -42,13 +44,14 @@ y_train = y_train[:validation_index]
 x_train = np.c_[np.ones(x_train.shape[0]), x_train]
 x_valid = np.c_[np.ones(x_valid.shape[0]), x_valid]
 
+#crossval = KFold(n_splits=10, random_state=1, shuffle=True)
 
 def neural():
     nn = NeuralNetwork(learning_rate=0.001, epochs=1000, lmd=1, layers=[x_train.shape[1], 100, 1])
 
     nn.fit(x_train, y_train)
 
-    nn.plot_loss()
+    # nn.plot_loss()
 
     preds = nn.predict(x_test)
 
@@ -59,6 +62,8 @@ def logistic():
     logistic = LogisticRegression(n_features=x_train.shape[1], learning_rate=0.005, n_steps=1000, lmd=8)
     cost_history, cost_history_val = logistic.fit_reg(x_train, y_train, x_valid, y_valid)
 
+    #scores = cross_val_score(logistic, x_train, y_train, scoring='accuracy', cv=10, n_jobs=1)
+    #print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
 
     print(f'''Thetas: {*logistic.theta,}''')
     print(f'''Final train BCE {cost_history[-1]:.3f}''')
